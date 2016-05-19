a5ddb6a4b86f01f95d637e290cfb1f67d1449b6a
mrmahmoud18 <mrmahmoud181818@yahoo.com>
2016-05-19 15:57:23 +0200

    just a pause

--- PROJECT/PROJECT/Simulation.h
+++ PROJECT/PROJECT/Simulation.h
@@ -2,8 +2,11 @@
 
 #include<vector>
 #include<numeric>
+#include <sstream>
 
 class Component;
+class LED;
+class SWITCH;
 
 class Simulation
 {
@@ -13,5 +16,12 @@ public:
 	static bool IsValidToSimulate(std::vector<Component*> ComponentList);
 	static void Simulate(std::vector<Component*> ComponentList);
 	static void ClearStatus(std::vector<Component*> ComponentList);
+	//static void TruthTable(std::vector<Component*> ComponentList);
+	static void Simulation::CreatePossibilites(int NumberOfSwitches, std::string Possibilities = "");
+	static std::vector<Pin::Status> StringToStatus(std::string s);
+	static std::vector<SWITCH*> GetSWITCHList();
+	static std::vector<LED*> GetLEDList();
+	static void SetSWITCHStatus(std::vector<SWITCH*> v ,std::vector<Pin::Status>Values);
+	static void GetLEDStatus(std::vector<LED*> v);
 };
 
